391c2509887804bbf81979dad2a62673fc91e2cc
cursophplms <cursophplms@gmail.com>
2020-05-25 13:16:40 -0300

    criando o PageAdmin e o TplAdmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 //use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
@@ -15,6 +16,17 @@ $app->get('/', function() {
     
 
 	$page = new Page(); // neste momento ele vai chamar o construct e vai chamar o header.html na tela muito loco
+
+	$page->setTpl("index");
+
+	
+});
+
+// segunda rota
+$app->get('/admin', function() {
+    
+
+	$page = new PageAdmin(); // neste momento ele vai chamar o construct e vai chamar o header.html na tela muito loco
 
 	$page->setTpl("index");
 
